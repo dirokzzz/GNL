96cb37611b30495fcbf19cb81c18b6a96ac8d3b0
Batson Colossus <bcolossu@ga-e2.21-school.ru>
2019-10-14 19:43:56 +0300

    123

--- get_next_line.h
+++ get_next_line.h
@@ -1,6 +1,13 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+typedef struct s_gnl
+{
+    int     fd;
+    char    *segment;
+    struct  s_gnl *next;
+}              t_gnl;
+
 #include <stdio.h>
 #include "libft/libft.h"
 #include <fcntl.h>
